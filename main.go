24005fbe5d6368d74b2ffd1ac6d556f6be32f6b2
Max Peng <pengcheng95@gmail.com>
2019-01-14 22:33:07 -0500

    Add get all tasks endpoint

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Task struct {
+	Title string `json:"Title"`
+}
+
+type Tasks []Task
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
+func returnAllTasks(w http.ResponseWriter, r *http.Request) {
+	tasks := Tasks{
+		Task{Title: "Task 1"},
+		Task{Title: "Task 2"},
+	}
+
+	fmt.Println("Endpoint hit: returnAllTasks")
+
+	json.NewEncoder(w).Encode(tasks)
+}
+
 func main() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/all", returnAllTasks)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
